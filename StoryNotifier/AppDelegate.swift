8f8d8ba5cbd63a919cefe177bbc03a9473d1d49b
CenoX <shlee3089@icloud.com>
2016-12-29 11:55:07 +0900

    Initial Commit

---
+++ StoryNotifier/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StoryNotifier
+//
+//  Created by CenoX on 2016. 12. 29..
+//  Copyright © 2016년 Team Vanilla. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ StoryNotifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StoryNotifier
+//
+//  Created by CenoX on 2016. 12. 29..
+//  Copyright © 2016년 Team Vanilla. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
